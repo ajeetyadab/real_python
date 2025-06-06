d032ad017d6debf8738a3991e0d084233d11a905
ajeetyadab@gmail.com <@jIt4hero>
2025-06-06 07:41:27 +0530

    added exercise of chapter 5

---
+++ chapter_5.py
@@ -0,0 +1,25 @@
+# exercise_1
+# print the result of calculation 3** .125 as a fixes point number
+# with three decimal places
+
+# calculation = 3**.125
+# print(f"result are {calculation:.3f}")
+
+# print(round(calculation,3))
+
+
+#----------------------------
+
+#exercise_2
+# print the number 15000 as currency, with the thousands grouped with commas. currency should be displayed with two decimal places.
+# number = 150000
+# print(f"displaying the number {number:,}")
+#-----------------------------------------------------------------------
+
+# exercise_3
+
+# Print the result of 2/10 as a percentage with no decimal places. The output should look like 20%
+
+number = 2/10
+print(f"number is {number:.2%}")
+
