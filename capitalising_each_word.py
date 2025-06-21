e0159c39d7d5f6538cd1e6825f0eaa96f57b7f07
ajeetyadab@gmail.com <@jIt4hero>
2025-06-21 21:57:34 +0530

    add

---
+++ capitalising_each_word.py
@@ -0,0 +1,19 @@
+string = "hello world how are you"
+
+
+
+    
+def capitalised_string(string):
+    capitalised_strig = " "
+
+    splitted_string = string.split(" ")
+
+    capitalised_string = ""
+
+    for i in splitted_string:
+        random_var = i.capitalize()
+        capitalised_strig = capitalised_strig +" "+ random_var
+
+    return capitalised_strig
+
+print(capitalised_string(string))
